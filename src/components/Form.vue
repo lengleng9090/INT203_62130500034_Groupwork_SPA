93b8088c23923d5d259b01e5ada6afa4f62bbdec
UntillNIGHT <lengleng9090@gmail.com>
2021-04-12 15:48:42 +0700

    [ADD] 62130500034 add Form component

---
+++ src/components/Form.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="inline-block"/>
+    <form class="space-y-6 m-4 p-4 inline-block bg-blue-100 rounded">
+      <h1>เพิ่มรายการใหม่</h1>
+      <p>รายการ : <input type="text" class="border border-black rounded" /></p>
+      <p>รายรับ : <input type="text" class="border border-black rounded" /></p>
+      <p>รายจ่าย : <input type="text" class="border border-black rounded" /></p>
+      <p>วัน/เดือน/ปี : <input class="bg-red-200 rounded" type="date" /></p>
+      <input type="submit" value="ยืนยัน" />
+    </form>
+    <div class="inline-block"/>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,58 @@
 <template>
   <navigation-bar />
+  <div class="flex justify-between">
+    <Form/>
+  </div>
+  <table class="table-fixed mx-auto p-5">
+    <thead>
+      <tr>
+        <th class="w-1/2 ...">รายการ</th>
+        <th class="w-1/6 ...">รายรับ</th>
+        <th class="w-1/6 ...">รายจ่าย</th>
+        <th class="w-1/6 ...">วัน/เดือน/ปี</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="space-x-3">
+          <button>
+          <img src="../../public/img/unchecked.png" width="20" class="inline-block"> 
+          </button>
+          <button>
+          <img src="../../public/img/edit.png" width="20" class="inline-block"> 
+          </button>
+          to CSS
+          </td>
+        <td id="tdcontent">Adam</td>
+        <td id="tdcontent">858</td>
+        <td id="tdcontent"></td>
+      </tr>
+      <tr class="bg-red-200">
+        <td>
+          A Long and Winding Tour of the History of UI Frameworks and Tools and
+          the Impact on Design
+        </td>
+        <td id="tdcontent">-</td>
+        <td id="tdcontent">200</td>
+        <td id="tdcontent"></td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import Form from '@/components/Form.vue'
 
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Form
+  },
 };
 </script>
+<style scoped>
+#tdcontent {
+  text-align: center;
+}
+</style>
